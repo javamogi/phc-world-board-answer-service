27d7eadc66ac2cf356187517d5b28eb90d200131
javamogi <javamogi@gmail.com>
2024-01-18 10:20:52 +0900

    refactor: webclient 통신 코드 수정

--- src/main/java/com/phcworld/boardanswerservice/service/AnswerService.java
+++ src/main/java/com/phcworld/boardanswerservice/service/AnswerService.java
@@ -45,30 +45,13 @@ public class AnswerService {
 	private final WebClient.Builder webClient;
 	private final KafkaProducer kafkaProducer;
 	private final AnswerProducer answerProducer;
-	private final CircuitBreakerFactory circuitBreakerFactory;
+//	private final CircuitBreakerFactory circuitBreakerFactory;
+	private final WebclientService webclientService;
 
 
 	public AnswerResponseDto register(AnswerRequestDto request, String token) {
 
-		log.info("Before call boards microservice");
-		CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitbreaker");
-		boolean existedFreeBoard = Boolean.TRUE.equals(
-				circuitBreaker.run(() ->
-				webClient.build()
-				.mutate().baseUrl("http://localhost:8080/boards")
-//				.mutate().baseUrl(env.getProperty("board_service.url"))
-				.build()
-				.get()
-				.uri(uriBuilder -> uriBuilder
-						.path("/{id}/exist")
-						.build(request.boardId()))
-				.header("Authorization", token)
-				.retrieve()
-				.bodyToMono(Boolean.class)
-				.block(),
-						throwable -> false)
-		);
-		log.info("After call boards microservice");
+		boolean existedFreeBoard = webclientService.existFreeBoard(request, token);
 
 		if (!existedFreeBoard) {
 			throw new NotFoundException();
@@ -94,17 +77,7 @@ public class AnswerService {
 		kafkaProducer.send("board-topic", freeBoardAnswer);
 		answerProducer.send("answers", freeBoardAnswer);
 
-		log.info("Before call users microservice");
-		UserResponseDto user = circuitBreaker.run(
-				() -> getUserResponseDto(token, freeBoardAnswer.getWriterId()),
-				throwable -> UserResponseDto.builder()
-						.email("")
-						.name("")
-						.createDate("")
-						.profileImage("")
-						.userId("")
-						.build());
-		log.info("After called users microservice");
+		UserResponseDto user = webclientService.getUserResponseDto(token, freeBoardAnswer);
 
 		if(user == null){
 			throw new NotFoundException();
@@ -118,38 +91,11 @@ public class AnswerService {
 				.build();
 	}
 
-	@Nullable
-	private UserResponseDto getUserResponseDto(String token, String userId) {
-		return webClient.build()
-				.mutate().baseUrl("http://localhost:8080/users")
-//				.mutate().baseUrl(env.getProperty("user_service.url"))
-				.build()
-				.get()
-				.uri(uriBuilder -> uriBuilder
-						.path("/{id}")
-						.build(userId))
-				.header("Authorization", token)
-				.retrieve()
-				.bodyToMono(UserResponseDto.class)
-				.block();
-	}
-
 	public AnswerResponseDto getFreeBoardAnswer(String answerId, String token) {
 		FreeBoardAnswer freeBoardAnswer = freeBoardAnswerRepository.findByAnswerId(answerId)
 				.orElseThrow(NotFoundException::new);
 
-		log.info("Before call users microservice");
-		CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitbreaker");
-		UserResponseDto user = circuitBreaker.run(
-				() -> getUserResponseDto(token, freeBoardAnswer.getWriterId()),
-				throwable -> UserResponseDto.builder()
-						.email("")
-						.name("")
-						.createDate("")
-						.profileImage("")
-						.userId("")
-						.build());
-		log.info("After called users microservice");
+		UserResponseDto user = webclientService.getUserResponseDto(token, freeBoardAnswer);
 
 		return AnswerResponseDto.builder()
 				.answerId(freeBoardAnswer.getAnswerId())
@@ -171,18 +117,7 @@ public class AnswerService {
 
 		answer.update(request.contents());
 
-		log.info("Before call users microservice");
-		CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitbreaker");
-		UserResponseDto user = circuitBreaker.run(
-				() -> getUserResponseDto(token, answer.getWriterId()),
-				throwable -> UserResponseDto.builder()
-						.email("")
-						.name("")
-						.createDate("")
-						.profileImage("")
-						.userId("")
-						.build());
-		log.info("After called users microservice");
+		UserResponseDto user = webclientService.getUserResponseDto(token, answer);
 
 		return  AnswerResponseDto.builder()
 				.answerId(answer.getAnswerId())
@@ -218,12 +153,7 @@ public class AnswerService {
 				.distinct()
 				.toList();
 
-		log.info("Before call users microservice");
-		CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitbreaker");
-		Map<String, UserResponseDto> users = circuitBreaker.run(
-				() -> getUserResponseDtoMap(token, userIds),
-				throwable -> new HashMap<>());
-		log.info("After called users microservice");
+		Map<String, UserResponseDto> users = webclientService.getUserResponseDtoMap(token, userIds);
 
 		UserResponseDto user = UserResponseDto.builder()
 				.email("")
@@ -243,23 +173,6 @@ public class AnswerService {
 							.build();
 				})
 				.toList();
-	}
-
-	@Nullable
-	private Map<String, UserResponseDto> getUserResponseDtoMap(String token, List<String> userIds) {
-		return webClient.build()
-				.mutate().baseUrl("http://localhost:8080/users")
-//				.mutate().baseUrl(env.getProperty("user_service.url"))
-				.build()
-				.get()
-				.uri(uriBuilder -> uriBuilder
-						.path("")
-						.queryParam("userIds", userIds)
-						.build())
-				.header(HttpHeaders.AUTHORIZATION, token)
-				.retrieve()
-				.bodyToMono(new ParameterizedTypeReference<Map<String, UserResponseDto>>() {})
-				.block();
 	}
 
 }
